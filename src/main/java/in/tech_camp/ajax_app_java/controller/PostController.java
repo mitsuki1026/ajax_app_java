945f82db2709f61c685e8eca4e5217cb3a69adc2
mitsuki1026 <mitsuki.ito@smart-group.co.jp>
2026-05-26 15:29:11 +0900

    リクエストを処理する

--- src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
+++ src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
@@ -1,5 +1,6 @@
 package in.tech_camp.ajax_app_java.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
